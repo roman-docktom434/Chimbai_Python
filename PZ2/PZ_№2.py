c52687a7fb001168dc4a8a61ef4b734c126b93c1
Beylax <62020511+roman-docktom434@users.noreply.github.com>
2024-11-01 17:17:00 +0300

    Delete PZ2/PZ_№2.py

--- PZ2/PZ_№2.py
+++
@@ -1,9 +0,0 @@
-m = input("Введите массу в килограммах: ")
-while type(m) != int:
-        try:
-            m = int(m)
-        except ValueError:
-            print("Ошибка! Введите число!")
-            m = input("Введите массу в килограммах: ")
-tons = m // 1000
-print("Полных тонн: ", tons)
