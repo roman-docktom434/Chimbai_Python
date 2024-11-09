7d9ace7f36c15f69e72925ecf6f92ca29f14718f
Beylax <62020511+roman-docktom434@users.noreply.github.com>
2024-11-09 13:14:53 +0300

    Update PZ3_task2.py

--- pz_3/PZ3_task2.py
+++ pz_3/PZ3_task2.py
@@ -1,13 +1,20 @@
+'''
+Единицы массы пронумерованы следующим образом: 1 — килограмм, 2 — 
+миллиграмм, 3 — грамм, 4 — тонна, 5 — центнер. Дан номер единицы массы (целое 
+число в диапазоне 1-5) и масса тела в этих единицах (вещественное число). Найти 
+массу тела в килограммах.
+'''
+
 kil, mil, gr, tonna, centner = '1 - килограмм', '2 - миллиграмм', '3 - грамм', '4 - тонна', '5 - центнер'
 massa1 = input("Введите свое ЦЕЛОЕ число: ")
-while type(massa1) != int:
+while type(massa1) != int: #Обработка исключений
     try:
         massa1 = int(massa1)
     except ValueError:
         print('Вы ввели не число!')
         massa1 = input("Введите свое ЦЕЛОЕ число: ")
 u_massa = input(f'Какую единицу массы Вы переводите в килограммы?\n{kil.title()}\n{mil.title()}\n{gr.title()}\n{tonna.title()}\n{centner.title()}\nВведите цифру: ')
-while type(u_massa) != int:
+while type(u_massa) != int: #Обработка исключений
     try:
         u_massa = int(u_massa)
     except ValueError:
