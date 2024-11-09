f060794bb7bb4ef679d9cedb08da6b7eb5900fbc
Beylax <62020511+roman-docktom434@users.noreply.github.com>
2024-11-09 13:15:08 +0300

    Update PZ3_task1.py

--- pz_3/PZ3_task1.py
+++ pz_3/PZ3_task1.py
@@ -4,7 +4,7 @@
 '''
 
 a,b,c = input('Введите число A: '), input('Введите число B: '), input('Введите число C: ')
-while type(a) != int and type(b) != int and type(c) != int:
+while type(a) != int and type(b) != int and type(c) != int: #Обработка исключений
     try:
         a = int(a)
         b = int(b)
